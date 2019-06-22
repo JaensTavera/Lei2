a2462a86eb783334cf676fafad554742807680e3
JaensTavera <JaensTavera@users.noreply.github.com>
2019-06-22 09:40:24 -0400

    Primer Commit de Lei2
    Aplicacion en Blanco

---
+++ Lei2Solucion/Lei2Projecto/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Lei2Projecto.Areas.Identity.Data;
+using Lei2Projecto.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Lei2Projecto.Areas.Identity.IdentityHostingStartup))]
+namespace Lei2Projecto.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DataContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DataContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>()
+                    .AddEntityFrameworkStores<DataContext>();
+            });
+        }
+    }
+}
